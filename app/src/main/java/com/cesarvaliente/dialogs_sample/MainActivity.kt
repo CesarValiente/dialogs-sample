1cb55ea613df3f89b8ec4dedd4296d928b0ed42f
Cesar Valiente <cevalien@microsoft.com>
2022-01-18 14:32:29 +0100

    Added popup menu sample

--- app/src/main/java/com/cesarvaliente/dialogs_sample/MainActivity.kt
+++ app/src/main/java/com/cesarvaliente/dialogs_sample/MainActivity.kt
@@ -3,7 +3,10 @@ package com.cesarvaliente.dialogs_sample
 import android.app.AlertDialog
 import android.os.Bundle
 import android.view.Gravity
+import android.view.MenuInflater
+import android.view.View
 import android.view.WindowManager
+import android.widget.PopupMenu
 import androidx.appcompat.app.AppCompatActivity
 import com.cesarvaliente.dialogs_sample.databinding.ActivityMainBinding
 
@@ -18,6 +21,7 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        // Dialogs
         binding.showDialogLeft.setOnClickListener {
             showDialog(Side.LEFT)
         }
@@ -26,6 +30,17 @@ class MainActivity : AppCompatActivity() {
         }
         binding.showDialogRight.setOnClickListener {
             showDialog(Side.RIGHT)
+        }
+
+        //PopUpMenu
+        binding.contextMenuLeft.setOnClickListener {
+            showPopUpMenu(it, Side.LEFT)
+        }
+        binding.contextMenuCenter.setOnClickListener {
+            showPopUpMenu(it, Side.CENTER)
+        }
+        binding.contextMenuRight.setOnClickListener {
+            showPopUpMenu(it, Side.RIGHT)
         }
     }
 
@@ -45,5 +60,18 @@ class MainActivity : AppCompatActivity() {
             window.attributes = wlp
         }
         dialog.show()
+    }
+
+    private fun showPopUpMenu(view: View, side: Side) {
+        val gravity = when (side) {
+            Side.LEFT -> Gravity.START
+            Side.RIGHT -> Gravity.END
+            else -> Gravity.CENTER
+        }
+        val popup = PopupMenu(this, view, gravity)
+        val inflater: MenuInflater = popup.menuInflater
+        inflater.inflate(R.menu.menu_layout, popup.menu)
+
+        popup.show()
     }
 }
